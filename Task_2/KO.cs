483579257d0c12cd616312c6274db245f71a8583
Tsinana <hlebusru@gmail.com>
2021-12-26 16:01:22 +0300

    На данный момент создания второй проги имеем

---
+++ Task_2/KO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class KO
+    {
+        StreamWriter SW;
+        private List<string> set = new List<string>();
+        private List<string> set_test = new List<string>();
+        private int n;
+
+        public KO(StreamWriter SW)
+        {
+            set.Add("a"); set.Add("b"); set.Add("c"); set.Add("d"); set.Add("e"); set.Add("f");
+            set.Add("a"); set.Add("a"); set.Add(" "); set.Add(" "); set.Add(" "); set.Add(" ");
+            n = set.Count;
+            this.SW = SW;
+        }
+        public void Generating_all_combinations_of_k_elements(int k)
+        {
+            List<string> list_st = new List<string>();
+            int index = 0;
+            Generating_all_combinations_of_k_elements(0, -1, list_st, n, k, ref index);
+        }
+
+        private void Generating_all_combinations_of_k_elements(int num, int last, List<string> list_st, int n, int m, ref int index)
+        {
+            if (num == m)
+            {
+                SW.Write(index++ + ". ");
+                for (int i = 0; i < m; i++)
+                {
+                    SW.Write(list_st[i] + " ");
+                }
+                SW.WriteLine();
+            }
+            else
+            {
+                for (int i = last + 1; i < n; i++)
+                {
+                    list_st.Add(set_test[i]);
+                    Generating_all_combinations_of_k_elements(num + 1, i, list_st, n, m, ref index);
+                    list_st.Remove(set_test[i]); ;
+                }
+            }
+        }
+    }
+}
---
+++ Task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Далее в программе весь результат мы будем сохранять в файл. Укажите путь для сохранения файла.\nВ какой каталог сохраним файл:");
+            string st1 = Console.ReadLine();
+            Console.WriteLine("Как будет назван файл:");
+            string st2 = Console.ReadLine();
+            string way = st1 + ":/" + st2 + ".txt";
+            StreamWriter SW = SW = new StreamWriter(@way);
+            Console.WriteLine("Для продолжения нажмите любую клавишу . . .");
+            Console.ReadKey();
+            Console.Clear();;
+            KO ko = new KO(SW);
+            ko.Generating_all_combinations_of_k_elements(5);
+
+
+
+
+
+
+
+
+            SW.Close();
+        }
+    }
+}
